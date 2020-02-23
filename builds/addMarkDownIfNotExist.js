a7c5f6ad8c71ed2917b0d5c7b821ac68eabc9012
meidanlong <meidanlong@qq.com>
2020-02-23 20:22:48 +0800

    1. 博客-React-15到17章 2. 优化评论组件只有在之前版本没有时才添加

---
+++ builds/addMarkDownIfNotExist.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const findMarkdown = require('./findMarkdown')
+const rootDir = './docs'
+
+findMarkdown(rootDir, writeComponents)
+
+function writeComponents(dir) {
+    // 禁止在引导页面添加评论
+    var namearray = dir.split('README')
+    if (namearray.length === 1) {
+        fs.readFile(dir, 'utf-8', (err, content) => {
+            if (err) throw err
+
+            if(content.split('<comment/>').length > 1){
+                // 说明已包含评论组件
+            }else{
+                fs.appendFile(dir, `<comment/>`, err => {
+                    if (err) throw err
+                    // console.log(`add components to ${dir}`)
+                })
+            }
+        })
+
+    }
+}
